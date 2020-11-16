e00aabbc7bdf6ec51b84224bf52ee3ffa58d8ee7
Murtaza Akbari <murtazasakbari@gmail.com>
2020-11-16 13:06:22 +0530

    Publish to Clojars upon publishing a release on Github

--- project.clj
+++ project.clj
@@ -5,9 +5,12 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [honeysql "1.0.444"]]
+  :deploy-repositories [["clojars" {:url      "https://clojars.org/nilenso/honeysql-postgres"
+                                    :username :env/CLOJARS_USERNANE
+                                    :password :env/CLOJARS_DEPLOY_TOKEN}]]
   :plugins [[lein-cljfmt "0.7.0"]]
   :tach {:test-runner-ns 'honeysql-postgres.postgres-test
          :source-paths   ["src" "test"]}
   :profiles {:dev {:plugins [[lein-tach "0.4.0"]]}
-             :ci {:dependencies [[seancorfield/readme "1.0.16"]]
-                  :aliases {"verify-readme" ["run" "-m" "seancorfield.readme"]}}})
+             :ci  {:dependencies [[seancorfield/readme "1.0.16"]]
+                   :aliases      {"verify-readme" ["run" "-m" "seancorfield.readme"]}}})
